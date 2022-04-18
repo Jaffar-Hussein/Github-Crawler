431f07726fc28c6d6d149a66b866409ac45f1f36
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-18 04:32:52 +0200

    ft:fixed the working of the nav-bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GitInfoService } from '../git-info.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent implements OnInit {
-
-  constructor() { }
+  inputData!:string;
+  constructor(private gitinfo:GitInfoService,public router:Router) { }
 
   ngOnInit(): void {
+  }
+  userSearch() {
+    this.gitinfo.userApi(`https://api.github.com/users/${this.inputData}`)
+    this.router.navigate(['user'])
+  }
+  repoSearch() {
+    // this.gitinfo.repoApi(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=10`)
+    this.router.navigate(['repositories'])
   }
 
 }
