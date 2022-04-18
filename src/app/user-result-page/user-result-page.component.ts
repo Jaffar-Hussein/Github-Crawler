7cb5cfb2a79af1743707bb01a4bc0d110478558b
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-18 04:10:11 +0200

    fft

--- src/app/user-result-page/user-result-page.component.ts
+++ src/app/user-result-page/user-result-page.component.ts
@@ -9,15 +9,15 @@ import { UserSkeleton } from '../user-skeleton';
   styleUrls: ['./user-result-page.component.css']
 })
 export class UserResultPageComponent implements OnInit {
-  bio: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, dignissimos.
-  Exercitationem voluptas atque veritatis deleniti perferendis eveniet harum ipsum magni!
-  Ad eos error a nobis veniam omnis provident ipsam cumque!
-  Qui ea sint, iure distinctio quia quasi cum iste quis.
-  Recusandae molestias quidem ab ullam facilis deleniti! Quasi, laudantium expedita!`;
-  name!: string
-  followers: number = 0;
-  following: number = 0;
-  publicRepositories: number = 0;
+  // bio: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, dignissimos.
+  // Exercitationem voluptas atque veritatis deleniti perferendis eveniet harum ipsum magni!
+  // Ad eos error a nobis veniam omnis provident ipsam cumque!
+  // Qui ea sint, iure distinctio quia quasi cum iste quis.
+  // Recusandae molestias quidem ab ullam facilis deleniti! Quasi, laudantium expedita!`;
+  // name!: string
+  // followers: number = 0;
+  // following: number = 0;
+  // publicRepositories: number = 0;
   htmlURL!: string;
   user: any = [];
   users!: UserSkeleton
@@ -32,7 +32,7 @@ export class UserResultPageComponent implements OnInit {
     this.users = this.gitinfo.userSkeleton
     this.repo = this.gitinfo.repos
     // this.repo.push(this.userRespo);
-    // console.log("repo", this.repo);
+    console.log("repo", this.repo);
 
     this.user.push(this.users)
     // console.log(this.user);
