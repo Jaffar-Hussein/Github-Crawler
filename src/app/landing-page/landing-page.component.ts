602f0872f193796e3ef8223ba7c886f726406231
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-19 00:56:36 +0300

    ft:validation-form validation

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -15,11 +15,19 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
   }
   userSearch() {
-    
+    if(this.inputData===''){
+      alert("Search field cannot be left empty");
+
+      return;
+    }
     this.gitinfo.userApi(`https://api.github.com/users/${this.inputData}`)
     this.router.navigate(['user'])
   }
   repoSearch() {
+    if(this.inputData===''){
+      alert("Search field cannot be left empty");
+      return;
+    }
     // this.gitinfo.repoApi(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=10`)
     this.router.navigate(['repositories'])
     this.gitinfo.userRepoSearch(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=17`)
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -13,6 +13,11 @@ export class NavigationBarComponent implements OnInit {
   ngOnInit(): void {
   }
   userSearch() {
+    if(this.inputData===''){
+      alert("Search field cannot be left empty");
+
+      return;
+    }
     if(true)
     this.gitinfo.userApi(`https://api.github.com/users/${this.inputData}`)
     this.router.navigate(['user'])
@@ -21,6 +26,11 @@ export class NavigationBarComponent implements OnInit {
     this.router.navigate([''])
   }
   repoSearch() {
+    if(this.inputData===''){
+      alert("Search field cannot be left empty");
+
+      return;
+    }
     // this.gitinfo.repoApi(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=10`)
     this.router.navigate(['repositories'])
     this.gitinfo.userRepoSearch(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=17`)
